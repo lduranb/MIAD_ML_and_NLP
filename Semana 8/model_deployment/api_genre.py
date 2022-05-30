0355ff2beaa36245917b9f0cc707d9c997f5542c
lduranb <93063244+lduranb@users.noreply.github.com>
2022-05-29 23:32:23 -0500

    Add files via upload

---
+++ Semana 8/model_deployment/api_genre.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restx import Api, Resource, fields
+import joblib
+from model_deployment import predict_genre
+
+# Definición aplicación Flask
+app = Flask(__name__)
+
+# Definición API Flask
+api = Api(
+    app, 
+    version='1.0', 
+    title='Movie genre from plot',
+    description='Predict genres probability from movie plot')
+
+ns = api.namespace('predict', 
+     description='Movie Genre Predictor')
+
+# Definición argumentos o parámetros de la API
+parser = api.parser()
+parser.add_argument(
+    'Plot', 
+    type=str, 
+    required=True, 
+    help='Data to be analyzed', 
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+# Definición de la clase para disponibilización
+@ns.route('/')
+class PhishingApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": predict_genre(args['Plot'])
+        }, 200
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
---
+++ Semana 8/model_deployment/model_deployment.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+%pip install neattext
+%pip install flask
+%pip install flask_restx
+
+from flask import Flask
+from flask_restx import Api, Resource, fields
+import sys
+import os
+import joblib
+import pandas as pd
+
+import neattext as nt
+import neattext.functions as nfx
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def predict_genre(plot):
+    
+    tfidf = joblib.load('model_deployment/Xfeat.pkl') 
+    clf = joblib.load('model_deployment/movie_genre_clf.pkl') 
+
+    plot_ = pd.DataFrame([plot], columns=['plot'])
+  
+    # Create features
+    plot_['plot'].apply(lambda x:nt.TextFrame(x).noise_scan())
+    plot_['plot'].apply(lambda x:nt.TextExtractor(x).extract_stopwords())
+    plot_['plot'].apply(nfx.remove_stopwords)
+    plot_['plot'].apply(nfx.remove_stopwords)
+
+    Xfeatures = tfidf.transform(plot_).toarray()
+ 
+    # Make prediction
+    p1 = clf.predict_proba(Xfeatures)
+
+    cols = ['p_Action', 'p_Adventure', 'p_Animation', 'p_Biography', 'p_Comedy', 'p_Crime', 'p_Documentary', 'p_Drama', 'p_Family',
+        'p_Fantasy', 'p_Film-Noir', 'p_History', 'p_Horror', 'p_Music', 'p_Musical', 'p_Mystery', 'p_News', 'p_Romance',
+        'p_Sci-Fi', 'p_Short', 'p_Sport', 'p_Thriller', 'p_War', 'p_Western']
+    
+    genre = pd.DataFrame(p1, columns=cols)
+    return genre.transpose().rename(columns = {0:'Proba'})
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please add an plot')
+        
+    else:
+
+        plot = sys.argv[1]
+
+        p1 = predict_genre(plot)
+        
+        print(plot)
+        print('Probability of genres: ', p1)
+        
